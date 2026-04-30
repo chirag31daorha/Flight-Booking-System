28a738979022bb7bc80c541e5384adf99eacd3a5
chirag31daorha <chirag2001daorha@gmail.com>
2026-04-30 23:34:17 +0530

    changes in corsconfg and securityconfig

--- src/main/java/com/jsp/flightbookingsystem/config/CorsConfig.java
+++ src/main/java/com/jsp/flightbookingsystem/config/CorsConfig.java
@@ -1,15 +1,24 @@
 package com.jsp.flightbookingsystem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
-            .allowedMethods("GET", "POST", "PUT", "DELETE");
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://flight-booking-front-end-rose.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
